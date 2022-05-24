3273c9ac19fbf24e38806b677e0305734f8b753e
ernstpf <phil@philernst.com>
2022-05-24 15:56:52 -0400

    Start UI stucture

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <form>
+        <fieldset>
+          <legend>What acrynym are you looking for?</legend>
+          <div class="inner-form">
+            <div class="input-field">
+              <button class="btn-search" type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                  <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
+                </svg>
+              </button>
+              <input id="search" type="text" placeholder="Enter Acronym" value="" />
+            </div>
+          </div>
+          <div class="suggestion-wrap">
+            <span>ROFL</span>
+            <span>LMK</span>
+            <span>NVM</span>
+            <span>TBH</span>
+            <span>SMH</span>
+          </div>
+        </fieldset>
+      </form>
     </div>
   );
 }
