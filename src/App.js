e4eb31dc3b0e5cc9d64e588fbe7b415013a63ea6
ernstpf <phil@philernst.com>
2022-05-25 16:29:00 -0400

    Add hook to accept input

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import './App.css';
 
+function handleChange(event) {
+  console.log(event.target.value);
+}
+
 function App() {
   return (
-    <div class="container">
+    <div className="container">
       <>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap" rel="stylesheet" />
       </>
       <form>
         <fieldset>
           <legend>What acrynym are you looking for?</legend>
-          <div class="inner-form">
-            <div class="input-field">
-              <button class="btn-search" type="button">
+          <div className="inner-form">
+            <div className="input-field">
+              <button className="btn-search" type="button">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                   <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                 </svg>
               </button>
-              <input id="search" type="text" placeholder="Enter Acronym" />
+              <input id="search" type="text" placeholder="Enter Acronym" onChange={handleChange} />
             </div>
           </div>
-          <div class="suggestion-wrap">
+          <div className="suggestion-wrap">
             <span>ROFL</span>
             <span>LMK</span>
             <span>NVM</span>
