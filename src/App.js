20d418fe389c69edc71be4f7c2f7808ac5c566a6
ernstpf <phil@philernst.com>
2022-05-31 09:51:06 -0400

    Refactor definitions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { useState } from "react";
-import React from 'react'
+import React from "react";
+import { definitions } from "./data/dictionary";
 
 function App() {
   const [searchValue, updateSearchValue] = useState("");
@@ -8,11 +9,7 @@ function App() {
   const [definition, updateDefinition] = useState("");
 
   const dictionary = new Map();
-  dictionary.set("ROFL", "Rolling on the Floor Laughing");
-  dictionary.set("LMK", "Let Me Know");
-  dictionary.set("NVM", "Never Mind");
-  dictionary.set("TBH", "To Be Honest");
-  dictionary.set("SMH", "Shaking My Head");
+  definitions.map((definition) => dictionary.set(definition.id, definition.value));
 
   function handleSuggestion(text) {
     updateSearchValue(text);
@@ -39,15 +36,8 @@ function App() {
     <div className="container">
       <>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap"
-          rel="stylesheet"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@800&display=swap" rel="stylesheet" />
       </>
       <form>
         <fieldset>
@@ -55,21 +45,11 @@ function App() {
           <div className="inner-form">
             <div className="input-field">
               <button className="btn-search" type="button">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                >
+                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
                   <path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z"></path>
                 </svg>
               </button>
-              <input
-                type="text"
-                onChange={handleChange}
-                value={searchValue}
-                placeholder="Enter Acronym"
-              />
+              <input type="text" onChange={handleChange} value={searchValue} placeholder="Enter Acronym" />
             </div>
           </div>
           <div className="suggestion-wrap">
@@ -82,7 +62,7 @@ function App() {
         </fieldset>
         <div className="results">
           <h2>{acronym}</h2>
-          <p>{definition}</p>
+          <span dangerouslySetInnerHTML={{ __html: definition }}></span>
         </div>
       </form>
     </div>
---
+++ src/data/dictionary.js
@@ -0,0 +1,30 @@
+export const definitions = [
+  {
+    id: "ROFL",
+    value:
+      "<p>ROFL is an internet accronmym for Rolling On Floor Laughing, and like all things internet it has adapted to nerd culture and has taken on new real-life use, though usually in a satirical way.</p><p>Typically a stronger, but equally as often ill-used version of lol.</p>",
+  },
+  {
+    id: "LMK",
+    value:
+      '<p>It simply means "let me know." Or get back to me with answer.</p>  <p>If it already formally exists, I don\'t know, but I started using it in my e-mails in the \'90s for convenience. Today, someone responded "OH!!!!!!" after I replied to his "what\'s the meaning of that" e-mail. So, there you have it.</p><p>In e-mail commonly, consider for example:</p><ol><li>If you need anything, LMK.</li><li>What\'re you guys doing tonight? LMK.</li></ol></p>',
+  },
+
+  {
+    id: "NVM",
+    value:
+      '<p>An abbreviation as "never mind" used mostly while texting</p>  <p>(texting)</p><p>-basceocj</p><p>-what???</p><p>-nvm</p>',
+  },
+
+  {
+    id: "TBH",
+    value:
+      "Tbh stands for “to be honest”. The word is most commonly used in texting and social media among kids and teens. In texting, whenever someone would give their honest opinion on something, they would say tbh as a short way to say to be honest. In social media however, it’s used when a follower tells an account owner to explain to them what they think of them, how they feel about them, etc. But it must be all 100% honest, whether it’s good or bad. So a tbh in social media is just giving your honest opinion to someone about them.  ",
+  },
+
+  {
+    id: "SMH",
+    value:
+      '<p>Meaning, "shaking my head", smh is typically used when something is obvious, plain old stupid, or disappointment.</p><p>You don\'t know what smh means? smh</p>',
+  },
+];
